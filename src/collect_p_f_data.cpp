e70e3beea9a3f1c33ba0c31bdd1546c14a537159
David P. Riedel <driedel@cox.net>
2013-04-23 14:03:31 -0400

    initial commit

---
+++ src/collect_p_f_data.cpp
@@ -0,0 +1,26 @@
+// =====================================================================================
+// 
+//       Filename:  collect_p_f_data.cpp
+// 
+//    Description:  This program will compute Point & Figure data for the
+//    				given input file.
+// 
+//        Version:  1.0
+//        Created:  04/23/2013 02:00:49 PM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  David P. Riedel (dpr), driedel@cox.net
+//        License:  GNU General Public License v3
+//        Company:  
+// 
+// =====================================================================================
+
+#include	<cstdlib>
+
+	int
+main ( int argc, char *argv[] )
+{
+	return EXIT_SUCCESS;
+}// ----------  end of function main  ----------
+
