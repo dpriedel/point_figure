a3096e5895349cb6e54f321003f30e0a5b321051
David P. Riedel <driedel@cox.net>
2013-04-24 14:41:28 -0400

    need app specific class.

---
+++ src/collect_p_f_data.h
@@ -0,0 +1,49 @@
+// =====================================================================================
+// 
+//       Filename:  collect_p_f_data.h
+// 
+//    Description:  Module to calculation Point & Figure data for a given symbol.
+// 
+//        Version:  1.0
+//        Created:  04/24/2013 02:06:24 PM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  David P. Riedel (dpr), driedel@cox.net
+//        License:  GNU General Public License v3
+//        Company:  
+// 
+// =====================================================================================
+
+// =====================================================================================
+//        Class:  CMyApp
+//  Description:  application specific stuff
+// =====================================================================================
+class CMyApp
+{
+	public:
+		// ====================  LIFECYCLE     =======================================
+		CMyApp (int argv, const char*[] argv);                             // constructor
+		~CMyApp(void);
+
+
+		// ====================  ACCESSORS     =======================================
+
+		// ====================  MUTATORS      =======================================
+
+		// ====================  OPERATORS     =======================================
+
+	protected:
+
+		virtual void Do_StartUp(void);
+		virtual void Do_CheckArgs(void);
+
+		virtual void Do_SetupProgramOptions(void);
+
+		// ====================  DATA MEMBERS  =======================================
+
+	private:
+		// ====================  DATA MEMBERS  =======================================
+
+}; // -----  end of class CMyApp  -----
+
