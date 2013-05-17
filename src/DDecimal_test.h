c155ddf03bf9ef5b13d63fd5dbff1b62023c8ae3
David P. Riedel <driedel@cox.net>
2013-05-17 10:14:52 -0400

    On branch master 	new file:   src/DDecimal_test.cpp 	new file:   src/DDecimal_test.h separate out code for testing DDecimal wrapper classes

---
+++ src/DDecimal_test.h
@@ -0,0 +1,67 @@
+// =====================================================================================
+// 
+//       Filename:  DDecimal_test.h
+// 
+//    Description:  Program to test c++ wrapper for libdecimal
+// 
+//        Version:  1.0
+//        Created:  05/17/2013 02:06:24 PM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  David P. Riedel (dpr), driedel@cox.net
+//        License:  GNU General Public License v3
+//        Company:  
+// 
+// =====================================================================================
+
+#include "CApplication.h"
+
+// =====================================================================================
+//        Class:  CMyApp
+//  Description:  application specific stuff
+// =====================================================================================
+class CMyApp : public CApplication
+{
+	public:
+		// ====================  LIFECYCLE     =======================================
+		CMyApp (int argc, char* argv[]);                             // constructor
+		~CMyApp(void);
+
+
+		// ====================  ACCESSORS     =======================================
+
+		// ====================  MUTATORS      =======================================
+
+		// ====================  OPERATORS     =======================================
+
+	protected:
+
+		virtual void Do_StartUp(void);
+		virtual void Do_CheckArgs(void);
+		virtual void Do_SetupProgramOptions(void);
+		virtual	void Do_ParseProgramOptions(void);
+		virtual void Do_Run(void);
+
+		// ====================  DATA MEMBERS  =======================================
+
+	private:
+		// ====================  DATA MEMBERS  =======================================
+	
+	fs::path mInputPath;
+	fs::path mOutputPath;
+	std::string mSymbol;
+	std::string mDBName;
+
+	enum class source { unknown, file, stdin };
+	enum class destination { unknown, DB, file, stdout };
+	enum class mode { unknown, load, update };
+
+	source mSource;
+	destination mDestination;
+	mode mMode;
+	bool mInputIsPath;
+	bool mOutputIsPath;
+
+}; // -----  end of class CMyApp  -----
+
