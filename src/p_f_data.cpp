3f8f5e5317cd975bed94b22a31ba07abbd815750
David P. Riedel <driedel@cox.net>
2013-05-21 14:04:14 -0400

    On branch master 	new file:   src/p_f_data.cpp 	new file:   src/p_f_data.h start build actual data management classes

---
+++ src/p_f_data.cpp
@@ -0,0 +1,30 @@
+// =====================================================================================
+// 
+//       Filename:  p_f_data.cpp
+// 
+//    Description:  Implementation of class which contains Point & Figure data for a symbol.
+// 
+//        Version:  1.0
+//        Created:  05/21/2013 01:58:39 PM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  David P. Riedel (dpr), driedel@cox.net
+//        License:  GNU General Public License v3
+//        Company:  
+// 
+// =====================================================================================
+
+
+//--------------------------------------------------------------------------------------
+//       Class:  P_F_Data
+//      Method:  P_F_Data
+// Description:  constructor
+//--------------------------------------------------------------------------------------
+
+P_F_Data::P_F_Data ()
+	: mBoxSize(0), mReversalBoxes(0), mCurrentDirection(direction::unknown)
+
+{
+}  // -----  end of method P_F_Data::P_F_Data  (constructor)  -----
+
---
+++ src/p_f_data.h
@@ -0,0 +1,70 @@
+// =====================================================================================
+// 
+//       Filename:  p_f_data.h
+// 
+//    Description:  class to manage Point & Figure data for a symbol.
+// 
+//        Version:  1.0
+//        Created:  04/30/2013 03:04:50 PM
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  David P. Riedel (dpr), driedel@cox.net
+//        License:  GNU General Public License v3
+//        Company:  
+// 
+// =====================================================================================
+
+// =====================================================================================
+//        Class:  P_F_Data
+//  Description:  class to manage Point & Figure data for a symbol.
+// =====================================================================================
+
+#include <boost/date_time/gregorian/gregorian.hpp>
+
+#include "DDecimal_16.h"
+
+namespace boost::gregorian greg;
+
+class P_F_Data
+{
+	public:
+		// ====================  LIFECYCLE     =======================================
+		P_F_Data ();                             // constructor
+		P_F_Data(const DDecimal<16> boxsize, int reversal);
+
+		// ====================  ACCESSORS     =======================================
+
+		// ====================  MUTATORS      =======================================
+		
+		void SetBoxSize(const DDecimal<16>& boxsize);
+		void SetReversal(int reversal);
+
+
+		// ====================  OPERATORS     =======================================
+
+	protected:
+		// ====================  DATA MEMBERS  =======================================
+
+	private:
+		// ====================  DATA MEMBERS  =======================================
+
+		Symbol mSymbol;
+
+		greg::date mFirstDate;			//	earliest entry for symbol
+		greg::date mLastChangeDate;		//	date of last change to data
+		greg::date mLastCheckedDate;	//	last time checked to see if update needed
+
+		DDecimal<16> mBoxSize;
+		int mReversalBoxes;
+
+		enum class direction { unknown, moving_up, moving_down };
+
+		direction mCurrentDirection;
+
+
+
+
+
+}; // -----  end of class P_F_Data  -----
+
