7cd0b322ce97cfdca124cb1c2bc70d24d2403682
David P. Riedel <davidp.riedel@gmail.com>
2021-08-06 09:36:40 -0400

    new module for streaming quotes
    - provide a generator interface

---
+++ src/LiveStream.cpp
@@ -0,0 +1,30 @@
+// =====================================================================================
+//
+//       Filename:  LiveStream.cpp
+//
+//    Description:  Live stream ticker updates 
+//
+//        Version:  1.0
+//        Created:  08/06/2021 09:28:55 AM
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  David P. Riedel (), driedel@cox.net
+//        License:  GNU General Public License -v3
+//
+// =====================================================================================
+
+
+#include "LiveStream.h"
+
+
+
+//--------------------------------------------------------------------------------------
+//       Class:  LiveStream
+//      Method:  LiveStream
+// Description:  constructor
+//--------------------------------------------------------------------------------------
+LiveStream::LiveStream ()
+{
+}  // -----  end of method LiveStream::LiveStream  (constructor)  ----- 
+
---
+++ src/LiveStream.h
@@ -0,0 +1,48 @@
+// =====================================================================================
+//
+//       Filename:  LiveStream.h
+//
+//    Description:  class to live stream ticker updatas 
+//
+//        Version:  1.0
+//        Created:  08/06/2021 09:26:57 AM
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  David P. Riedel (), driedel@cox.net
+//        License:  GNU General Public License -v3
+//
+// =====================================================================================
+
+#ifndef  LIVESTREAM_INC
+#define  LIVESTREAM_INC
+
+// =====================================================================================
+//        Class:  LiveStream
+//  Description:  live stream ticker updates -- look like a generator 
+// =====================================================================================
+class LiveStream
+{
+    public:
+        // ====================  LIFECYCLE     ======================================= 
+        LiveStream ();                             // constructor 
+
+        // ====================  ACCESSORS     ======================================= 
+
+        // ====================  MUTATORS      ======================================= 
+
+        // ====================  OPERATORS     ======================================= 
+
+    protected:
+        // ====================  METHODS       ======================================= 
+
+        // ====================  DATA MEMBERS  ======================================= 
+
+    private:
+        // ====================  METHODS       ======================================= 
+
+        // ====================  DATA MEMBERS  ======================================= 
+
+}; // -----  end of class LiveStream  ----- 
+
+#endif   // ----- #ifndef LIVESTREAM_INC  ----- 
