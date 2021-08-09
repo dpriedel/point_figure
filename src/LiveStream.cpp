f64e2f2fdc7289e91a4b53fbef8ce4a5a213cd8b
David P. Riedel <davidp.riedel@gmail.com>
2021-08-09 13:50:31 -0400

    get basic streaming support to work

--- src/LiveStream.cpp
+++ src/LiveStream.cpp
@@ -53,8 +53,9 @@ inline std::vector<T> split_string(std::string_view string_data, char delim)
 // Description:  constructor
 //--------------------------------------------------------------------------------------
 LiveStream::LiveStream (const std::string& host, const std::string& port, const std::string& prefix,
-            const std::string& symbols)
-    : host_{host}, port_{port}, websocket_prefix_{prefix}
+            const std::string& api_key, const std::string& symbols)
+    : api_key_{api_key}, host_{host}, port_{port}, websocket_prefix_{prefix},
+        ctx_{ssl::context::tlsv12_client}, resolver_{ioc_}, ws_{ioc_, ctx_}
 {
     // symbols is a string of one or more symbols to monitor.
     // If more than 1 symbol, list is coma delimited.
@@ -63,3 +64,81 @@ LiveStream::LiveStream (const std::string& host, const std::string& port, const 
 
 }  // -----  end of method LiveStream::LiveStream  (constructor)  ----- 
 
+void LiveStream::Connect()
+{
+    // Look up the domain name
+    auto const results = resolver_.resolve(host_, port_);
+
+    // Make the connection on the IP address we get from a lookup
+    auto ep = net::connect(get_lowest_layer(ws_), results);
+
+    // Set SNI Hostname (many hosts need this to handshake successfully)
+    if(! SSL_set_tlsext_host_name(ws_.next_layer().native_handle(), host_.c_str()))
+        throw beast::system_error(
+            beast::error_code(
+                static_cast<int>(::ERR_get_error()),
+                net::error::get_ssl_category()),
+            "Failed to set SNI Hostname");
+
+    // Update the host_ string. This will provide the value of the
+    // Host HTTP header during the WebSocket handshake.
+    // See https://tools.ietf.org/html/rfc7230#section-5.4
+    host_ += ':' + std::to_string(ep.port());
+
+    // Perform the SSL handshake
+    ws_.next_layer().handshake(ssl::stream_base::client);
+
+
+}
+void LiveStream::StartStreaming()
+{
+
+    // put this here for now.
+    // need to manually construct to get expected formate when serialized 
+    boost::json::array tickers = {"spy", "uso"};
+    for (const auto& symbol : symbol_list_)
+    {
+        tickers.push_back(boost::json::value(symbol));
+    }
+    boost::json::object event_data;
+    event_data["thresholdLevel"] = 5;
+    event_data["tickers"] = tickers;
+
+    boost::json::object obj;
+    obj["eventName"] = "subscribe";
+    obj["authorization"] = api_key_;
+    obj["eventData"] = event_data;
+
+    // std::cout << "obj: " << boost::json::serialize(obj) << '\n';
+
+    std::string const  text = boost::json::serialize(obj);
+
+    // Perform the websocket handshake
+    ws_.handshake(host_, websocket_prefix_);
+
+    // Send the message
+    ws_.write(net::buffer(text));
+
+    // This buffer will hold the incoming message
+    beast::flat_buffer buffer;
+    
+    int i = 0; 
+    while(++i < 10)
+    {
+        // Read a message into our buffer
+        ws_.read(buffer);
+        // The make_printable() function helps print a ConstBufferSequence
+        std::cout << beast::make_printable(buffer.data()) << std::endl;
+    }
+}
+
+void LiveStream::StopStreaming()
+{
+
+    // Close the WebSocket connection
+    ws_.close(websocket::close_code::normal);
+    // catch(std::exception const& e)
+    // {
+    //     std::cerr << "Error: " << e.what() << std::endl;
+    // }
+}
--- src/LiveStream.h
+++ src/LiveStream.h
@@ -20,6 +20,23 @@
 #include <string>
 #include <vector>
 
+#include <boost/beast/core.hpp>
+#include <boost/beast/ssl.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/websocket/ssl.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/stream.hpp>
+
+#include <boost/json.hpp>
+
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+
 #include "DDecDouble.h"
 
 // =====================================================================================
@@ -35,11 +52,16 @@ public:
     // ====================  LIFECYCLE     ======================================= 
     LiveStream () = default;                             // constructor 
     LiveStream (const std::string& host, const std::string& port, const std::string& prefix,
-            const std::string& symbols);
+            const std::string& api_key, const std::string& symbols);
 
     // ====================  ACCESSORS     ======================================= 
 
     // ====================  MUTATORS      ======================================= 
+
+    void Connect();
+    void Disconnect();
+    void StartStreaming();
+    void StopStreaming();
 
     // ====================  OPERATORS     ======================================= 
 
@@ -54,10 +76,15 @@ private:
     // ====================  DATA MEMBERS  ======================================= 
 
     std::vector<std::string> symbol_list_;
+    std::string api_key_;
     std::string host_;
     std::string port_;
     std::string websocket_prefix_;
 
+    net::io_context ioc_;
+    ssl::context ctx_;
+    tcp::resolver resolver_;
+    websocket::stream<beast::ssl_stream<tcp::socket>> ws_;
 }; // -----  end of class LiveStream  ----- 
 
 #endif   // ----- #ifndef LIVESTREAM_INC  ----- 
