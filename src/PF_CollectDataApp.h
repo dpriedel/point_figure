6d9fc746b6510b700ecc3f9e4e566f32ed0e06a9
David P. Riedel <davidp.riedel@gmail.com>
2022-02-21 09:56:11 -0500

    chore: Add license text and license file.
    Getting ready for first 'release' of the code.

--- src/PF_CollectDataApp.h
+++ src/PF_CollectDataApp.h
@@ -15,6 +15,22 @@
 // 
 // =====================================================================================
 
+
+	/* This file is part of PF_CollectData. */
+
+	/* PF_CollectData is free software: you can redistribute it and/or modify */
+	/* it under the terms of the GNU General Public License as published by */
+	/* the Free Software Foundation, either version 3 of the License, or */
+	/* (at your option) any later version. */
+
+	/* PF_CollectData is distributed in the hope that it will be useful, */
+	/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+	/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+	/* GNU General Public License for more details. */
+
+	/* You should have received a copy of the GNU General Public License */
+	/* along with PF_CollectData.  If not, see <http://www.gnu.org/licenses/>. */
+
 #ifndef  PF_COLLECTDATAAPP_INC
 #define  PF_COLLECTDATAAPP_INC
 
