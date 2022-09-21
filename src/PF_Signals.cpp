10fbe6a98293c307360f6e816baa854af34122a2
David P. Riedel <davidp.riedel@gmail.com>
2022-09-21 09:32:59 -0400

    feature: start work on find signals

---
+++ src/PF_Signals.cpp
@@ -0,0 +1,19 @@
+// =====================================================================================
+//
+//       Filename:  PF_Signals.cpp
+//
+//    Description:  Place for code related to finding trading 'signals'
+//                  in PF_Chart data.
+//
+//        Version:  1.0
+//        Created:  09/21/2022 09:27:22 AM
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  David P. Riedel (), driedel@cox.net
+//        License:  GNU General Public License -v3
+//
+// =====================================================================================
+
+#include "PF_Signals.h"
+
---
+++ src/PF_Signals.h
@@ -0,0 +1,22 @@
+// =====================================================================================
+//
+//       Filename:  PF_Signals.h
+//
+//    Description:  Place for code related to finding trading 'signals'
+//                  in PF_Chart data.
+//
+//        Version:  1.0
+//        Created:  09/21/2022 09:26:12 AM
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  David P. Riedel (), driedel@cox.net
+//        License:  GNU General Public License -v3
+//
+// =====================================================================================
+
+
+#ifndef  PF_SIGNALS_INC
+#define  PF_SIGNALS_INC
+
+#endif   // ----- #ifndef PF_SIGNALS_INC  ----- 
