521eee22731d5ec053e74197aa55b4997731817e
David P. Riedel <davidp.riedel@gmail.com>
2022-10-10 09:56:16 -0400

    feature: Signals
    basic drawing of tripletop buy and doublebottom sell signals

--- PF_DrawChart.py
+++ PF_DrawChart.py
@@ -1,39 +1,41 @@
-"""
-    Make this code into a module so it can be loaded at program
-    startup and parsed just once.
 
-    This module is called from a C++ program using an embedded Python
-    interpreter.  The C++ program creates a set of local variables which 
-    are made available to this code.
-"""
-
-"""
-	/* This file is part of PF_CollectData. */
-
-	/* PF_CollectData is free software: you can redistribute it and/or modify */
-	/* it under the terms of the GNU General Public License as published by */
-	/* the Free Software Foundation, either version 3 of the License, or */
-	/* (at your option) any later version. */
-
-	/* PF_CollectData is distributed in the hope that it will be useful, */
-	/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
-	/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
-	/* GNU General Public License for more details. */
-
-	/* You should have received a copy of the GNU General Public License */
-	/* along with PF_CollectData.  If not, see <http://www.gnu.org/licenses/>. */
-
-
-use axline to plot 45 degree trend lines
-
-"""
-
-import pandas as pd 
+import json
+import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 import mplfinance as mpf
 from matplotlib.ticker import ScalarFormatter
 import numpy as np
+
+"""
+    Make this code into a module so it can be loaded at program
+    startup and parsed just once.
+
+    This module is called from a C++ program using an embedded Python
+    interpreter.  The C++ program creates a set of local variables which
+    are made available to this code.
+"""
+
+"""
+    /* This file is part of PointAndFigure. */
+
+    /* PointAndFigure is free software: you can redistribute it and/or modify */
+    /* it under the terms of the GNU General Public License as published by */
+    /* the Free Software Foundation, either version 3 of the License, or */
+    /* (at your option) any later version. */
+
+    /* PointAndFigure is distributed in the hope that it will be useful, */
+    /* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+    /* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the */
+    /* GNU General Public License for more details. */
+
+    /* You should have received a copy of the GNU General Public License */
+    /* along with PointAndFigure.  If not, see <http://www.gnu.org/licenses/>. */
+
+
+use axline to plot 45 degree trend lines
+
+"""
 
 # load a non-GUI back end since we only produce graphic files as output
 
@@ -41,11 +43,11 @@ matplotlib.use("SVG")
 
 SLOPE = 0.707
 
-# some functions to use for trend lines 
+# some functions to use for trend lines
 
 
 def FindMinimum(is_up, chart_data):
-    # we expect a data_frame 
+    # we expect a data_frame
 
     minimum = 100000
     minimum_column = 0
@@ -64,7 +66,7 @@ def FindNextMaximum(data, start_at):
     pass
 
 
-def SetStepbackColor (is_up, stepped_back) : 
+def SetStepbackColor(is_up, stepped_back):
     if is_up:
         if stepped_back:
             return "blue"
@@ -73,7 +75,38 @@ def SetStepbackColor (is_up, stepped_back) :
             return "orange"
     return None
 
-def DrawChart(the_data, IsUp, StepBack, ChartTitle, ChartFileName, DateTimeFormat, ShowTrendLines, UseLogScale, Y_min, Y_max, openning_price):
+
+def DrawChart(the_data, ReversalBoxes, IsUp, StepBack, ChartTitle, ChartFileName, DateTimeFormat, ShowTrendLines,
+              UseLogScale, Y_min, Y_max, openning_price, signals):
+
+    # print(the_data)
+    signal_data = json.loads(signals)
+
+    # extract triple top buys
+    # need to have correct number of values in list
+    # must be same as number of rows columns in data
+    tt_buys = [np.nan] * len(the_data["Open"])
+    db_sells = [np.nan] * len(the_data["Open"])
+
+    had_tt_buy = False
+    had_db_sell = False
+
+    for sig in signal_data:
+        if sig["type"] == "tt_buy":
+            tt_buys[int(sig["column"])] = float(sig["box"])
+            had_tt_buy = True
+        if sig["type"] == "db_sell":
+            db_sells[int(sig["column"])] = float(sig["box"])
+            had_db_sell = True
+
+    apds = []
+    if ReversalBoxes > 1:
+        if had_tt_buy:
+            sig_buys = mpf.make_addplot(tt_buys, type="scatter", marker="^", color="black")
+            apds.append(sig_buys)
+        if had_db_sell:
+            sig_sells = mpf.make_addplot(db_sells, type="scatter", marker="v", color="black")
+            apds.append(sig_sells)
 
     chart_data = pd.DataFrame(the_data)
     chart_data["Date"] = pd.to_datetime(chart_data["Date"])
@@ -85,8 +118,8 @@ def DrawChart(the_data, IsUp, StepBack, ChartTitle, ChartFileName, DateTimeForma
     for i in range(len(IsUp)):
         mco.append(SetStepbackColor(IsUp[i], StepBack[i]))
 
-    mc = mpf.make_marketcolors(up='g',down='r')
-    s  = mpf.make_mpf_style(marketcolors=mc, gridstyle="dashed")
+    mc = mpf.make_marketcolors(up='g', down='r')
+    s = mpf.make_mpf_style(marketcolors=mc, gridstyle="dashed")
 
     # now generate a sequence of date pairs:
     # dates = chart_data["Date"]
@@ -96,21 +129,21 @@ def DrawChart(the_data, IsUp, StepBack, ChartTitle, ChartFileName, DateTimeForma
         ShowTrendLines = "no"
 
     if ShowTrendLines == "no":
-
         fig, axlist = mpf.plot(chart_data,
-            type="candle",
-            style=s,
-            marketcolor_overrides=mco,
-            title=ChartTitle,
-            figsize=(14, 10),
-            datetime_format=DateTimeFormat,
-            hlines=dict(hlines=[openning_price],colors=['r'],linestyle='dotted',linewidths=(2)),
-            returnfig=True)
+                               type="candle",
+                               style=s,
+                               marketcolor_overrides=mco,
+                               title=ChartTitle,
+                               figsize=(14, 10),
+                               datetime_format=DateTimeFormat,
+                               hlines=dict(hlines=[openning_price], colors=['r'], linestyle='dotted', linewidths=(2)),
+                               addplot=apds,
+                               returnfig=True)
 
     elif ShowTrendLines == "angle":
 
         # 45 degree trend line.
-        # need 2 points: first down column bottom and computed value for last column 
+        # need 2 points: first down column bottom and computed value for last column
 
         x1 = FindMinimum(IsUp, chart_data)
         y1 = chart_data.iloc[x1]["Low"]
@@ -122,31 +155,30 @@ def DrawChart(the_data, IsUp, StepBack, ChartTitle, ChartFileName, DateTimeForma
         y2 = SLOPE * (x2 - x1) + y1
         a_line_points = [(chart_data.iloc[x1]["Date"], y1), (chart_data.iloc[x2]["Date"], y2)]
         fig, axlist = mpf.plot(chart_data,
-            type="candle",
-            style=s,
-            marketcolor_overrides=mco,
-            title=ChartTitle,
-            figsize=(14, 10),
-            datetime_format=DateTimeFormat,
-            alines=a_line_points,
-            returnfig=True)
+                               type="candle",
+                               style=s,
+                               marketcolor_overrides=mco,
+                               title=ChartTitle,
+                               figsize=(14, 10),
+                               datetime_format=DateTimeFormat,
+                               alines=a_line_points,
+                               returnfig=True)
 
     else:
-
-        d1 = chart_data.index[ 0]
+        d1 = chart_data.index[0]
         d2 = chart_data.index[-1]
-        tdates = [(d1,d2)]
+        tdates = [(d1, d2)]
     
         fig, axlist = mpf.plot(chart_data,
-            type="candle",
-            style=s,
-            marketcolor_overrides=mco,
-            title=ChartTitle,
-            figsize=(14, 10),
-            datetime_format=DateTimeFormat,
-            tlines=[dict(tlines=tdates,tline_use='High',tline_method="point-to-point",colors='r'),
-                dict(tlines=tdates,tline_use='Low',tline_method="point-to-point",colors='b')],
-            returnfig=True)
+                               type="candle",
+                               style=s,
+                               marketcolor_overrides=mco,
+                               title=ChartTitle,
+                               figsize=(14, 10),
+                               datetime_format=DateTimeFormat,
+                               tlines=[dict(tlines=tdates, tline_use='High', tline_method="point-to-point", colors='r'),
+                                       dict(tlines=tdates, tline_use='Low', tline_method="point-to-point", colors='b')],
+                               returnfig=True)
 
     axlist[0].tick_params(which='both', left=True, right=True, labelright=True)
     axlist[0].secondary_xaxis('top')
@@ -156,12 +188,12 @@ def DrawChart(the_data, IsUp, StepBack, ChartTitle, ChartFileName, DateTimeForma
         axlist[0].set_yscale("log")
         axlist[0].grid(which='both', axis='both', ls='-')
         axlist[0].yaxis.set_major_formatter(ScalarFormatter())
-        axlist[0].yaxis.set_minor_formatter(ScalarFormatter()) 
+        axlist[0].yaxis.set_minor_formatter(ScalarFormatter())
 
     plt.savefig(ChartFileName)
     for ax in axlist:
         ax.clear()
-        del(ax)
+        del ax
     plt.close(fig)
-    del(axlist)
-    del(fig)
+    del axlist
+    del fig
