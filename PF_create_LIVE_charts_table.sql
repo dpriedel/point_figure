9e952ee6c95021be19d0b26609c8ef4e56b88417
David P. Riedel <davidp.riedel@gmail.com>
2023-10-18 09:27:36 -0400

    feature: track current signal in database
    - add column to track existence of an active signal for the stock.
    - split types into test and live versions so they can evolve separately.
    - make types owned by data_updater.
    - clean up formatting

--- PF_create_LIVE_charts_table.sql
+++ PF_create_LIVE_charts_table.sql
@@ -3,14 +3,33 @@
 CREATE SCHEMA IF NOT EXISTS live_point_and_figure;
 ALTER SCHEMA live_point_and_figure OWNER TO data_updater_pg;
 
-DROP TYPE IF EXISTS live_point_and_figure.DIRECTION CASCADE;
-CREATE TYPE live_point_and_figure.direction AS ENUM ('e_unknown', 'e_up', 'e_down');
+DROP TYPE IF EXISTS LIVE_DIRECTION CASCADE;
+CREATE TYPE live_direction AS ENUM ('e_unknown', 'e_up', 'e_down');
+ALTER TYPE live_direction OWNER TO data_updater_pg;
 
-DROP TYPE IF EXISTS live_point_and_figure.BOXTYPE CASCADE;
-CREATE TYPE live_point_and_figure.boxtype AS ENUM ('e_fractional', 'e_integral');
+DROP TYPE IF EXISTS LIVE_BOXTYPE CASCADE;
+CREATE TYPE live_boxtype AS ENUM ('e_fractional', 'e_integral');
+ALTER TYPE live_boxtype OWNER TO data_updater_pg;
 
-DROP TYPE IF EXISTS live_point_and_figure.BOXSCALE CASCADE;
-CREATE TYPE live_point_and_figure.boxscale AS ENUM ('e_linear', 'e_percent');
+DROP TYPE IF EXISTS LIVE_BOXSCALE CASCADE;
+CREATE TYPE live_boxscale AS ENUM ('e_linear', 'e_percent');
+ALTER TYPE live_boxscale OWNER TO data_updater_pg;
+
+DROP TYPE IF EXISTS LIVE_SIGNALTYPE CASCADE;
+CREATE TYPE live_signaltype AS ENUM (
+    'e_unknown',
+    'e_double_top_buy',
+    'e_double_bottom_sell',
+    'e_triple_top_buy',
+    'e_triple_bottom_sell',
+    'e_bullish_tt_buy',
+    'e_bearish_tb_sell',
+    'e_catapult_buy',
+    'e_catapult_sell',
+    'e_ttop_catapult_buy',
+    'e_tbottom_catapult_sell'
+);
+ALTER TYPE live_signaltype OWNER TO data_updater_pg;
 
 DROP TABLE IF EXISTS live_point_and_figure.pf_charts CASCADE;
 
@@ -21,17 +40,18 @@ CREATE TABLE live_point_and_figure.pf_charts
     fname_box_size NUMERIC(8, 4),
     chart_box_size NUMERIC(8, 4),
     reversal_boxes INTEGER,
-    box_type BOXTYPE,
-    box_scale BOXSCALE,
+    box_type LIVE_BOXTYPE,
+    box_scale LIVE_BOXSCALE,
     file_name TEXT NOT NULL,
     first_date TIMESTAMP NOT NULL,
     last_change_date TIMESTAMP NOT NULL,
     last_checked_date TIMESTAMP NOT NULL,
-    current_direction DIRECTION NOT NULL,
+    current_direction LIVE_DIRECTION NOT NULL,
+    current_signal LIVE_SIGNALTYPE NOT NULL,
     chart_data JSONB NOT NULL,
     cvs_graphics_data TEXT DEFAULT NULL,
-    UNIQUE(file_name),
-    PRIMARY KEY(file_name)
+    UNIQUE (file_name),
+    PRIMARY KEY (file_name)
 );
 
 ALTER TABLE live_point_and_figure.pf_charts OWNER TO data_updater_pg;
--- PF_create_TEST_charts_table.sql
+++ PF_create_TEST_charts_table.sql
@@ -3,14 +3,33 @@
 CREATE SCHEMA IF NOT EXISTS test_point_and_figure;
 ALTER SCHEMA test_point_and_figure OWNER TO data_updater_pg;
 
-DROP TYPE IF EXISTS test_point_and_figure.DIRECTION CASCADE;
-CREATE TYPE test_point_and_figure.direction AS ENUM ('e_unknown', 'e_up', 'e_down');
+DROP TYPE IF EXISTS TEST_DIRECTION CASCADE;
+CREATE TYPE test_direction AS ENUM ('e_unknown', 'e_up', 'e_down');
+ALTER TYPE test_direction OWNER TO data_updater_pg;
 
-DROP TYPE IF EXISTS test_point_and_figure.BOXTYPE CASCADE;
-CREATE TYPE test_point_and_figure.boxtype AS ENUM ('e_fractional', 'e_integral');
+DROP TYPE IF EXISTS TEST_BOXTYPE CASCADE;
+CREATE TYPE test_boxtype AS ENUM ('e_fractional', 'e_integral');
+ALTER TYPE test_boxtype OWNER TO data_updater_pg;
 
-DROP TYPE IF EXISTS test_point_and_figure.BOXSCALE CASCADE;
-CREATE TYPE test_point_and_figure.boxscale AS ENUM ('e_linear', 'e_percent');
+DROP TYPE IF EXISTS TEST_BOXSCALE CASCADE;
+CREATE TYPE test_boxscale AS ENUM ('e_linear', 'e_percent');
+ALTER TYPE test_boxscale OWNER TO data_updater_pg;
+
+DROP TYPE IF EXISTS TEST_SIGNALTYPE CASCADE;
+CREATE TYPE test_signaltype AS ENUM (
+    'e_unknown',
+    'e_double_top_buy',
+    'e_double_bottom_sell',
+    'e_triple_top_buy',
+    'e_triple_bottom_sell',
+    'e_bullish_tt_buy',
+    'e_bearish_tb_sell',
+    'e_catapult_buy',
+    'e_catapult_sell',
+    'e_ttop_catapult_buy',
+    'e_tbottom_catapult_sell'
+);
+ALTER TYPE test_signaltype OWNER TO data_updater_pg;
 
 DROP TABLE IF EXISTS test_point_and_figure.pf_charts CASCADE;
 
@@ -21,17 +40,18 @@ CREATE TABLE test_point_and_figure.pf_charts
     fname_box_size NUMERIC(8, 4),
     chart_box_size NUMERIC(8, 4),
     reversal_boxes INTEGER,
-    box_type BOXTYPE,
-    box_scale BOXSCALE,
+    box_type TEST_BOXTYPE,
+    box_scale TEST_BOXSCALE,
     file_name TEXT NOT NULL,
     first_date TIMESTAMP NOT NULL,
     last_change_date TIMESTAMP NOT NULL,
     last_checked_date TIMESTAMP NOT NULL,
-    current_direction DIRECTION NOT NULL,
+    current_direction TEST_DIRECTION NOT NULL,
+    current_signal TEST_SIGNALTYPE NOT NULL,
     chart_data JSONB NOT NULL,
     cvs_graphics_data TEXT DEFAULT NULL,
-    UNIQUE(file_name),
-    PRIMARY KEY(file_name)
+    UNIQUE (file_name),
+    PRIMARY KEY (file_name)
 );
 
 ALTER TABLE test_point_and_figure.pf_charts OWNER TO data_updater_pg;
