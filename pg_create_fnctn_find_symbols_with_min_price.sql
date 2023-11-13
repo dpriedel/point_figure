337ab7eaa8560ac94e617bd1b85e7130b0ee3971
David P. Riedel <davidp.riedel@gmail.com>
2023-11-13 15:04:07 -0500

    refactor: create/use new split_adj fields

--- pg_create_fnctn_find_symbols_with_min_price.sql
+++ pg_create_fnctn_find_symbols_with_min_price.sql
@@ -1,11 +1,11 @@
 -- this function will generate a list of symbols from the
--- specified exchange which have adjclose prices >= the supplied value
+-- specified exchange which have split_adj_close prices >= the supplied value
 -- checking from the given date.
 --
 
-DROP FUNCTION IF EXISTS new_stock_data.find_symbols_gte_min_adjclose(TEXT, NUMERIC, DATE);
+DROP FUNCTION IF EXISTS new_stock_data.find_symbols_gte_min_split_adj_close(TEXT, NUMERIC, DATE);
 
-CREATE OR REPLACE FUNCTION new_stock_data.find_symbols_gte_min_adjclose(
+CREATE OR REPLACE FUNCTION new_stock_data.find_symbols_gte_min_split_adj_close(
     IN xchng TEXT,
     IN min_close NUMERIC(14, 4) DEFAULT 5.0,
     IN start_date DATE DEFAULT current_date - interval '6 months'
@@ -26,7 +26,7 @@ BEGIN
             (
                 SELECT
                     t1.symbol,
-                    MIN(t1.adjclose) AS min_adjclose
+                    MIN(t1.split_adj_close) AS min_split_adj_close
                 FROM
                     new_stock_data.current_data AS t1
                 INNER JOIN new_stock_data.names_and_symbols AS t2
@@ -39,7 +39,7 @@ BEGIN
                     t1.symbol
             ) AS t3
         WHERE
-            t3.min_adjclose >= min_close
+            t3.min_split_adj_close >= min_close
         ORDER BY
             t3.symbol;
     RETURN;
@@ -50,4 +50,4 @@ LANGUAGE 'plpgsql' STABLE
 COST 100
 ROWS 2000;
 
-ALTER FUNCTION new_stock_data.find_symbols_gte_min_adjclose(TEXT, NUMERIC, DATE) OWNER TO data_updater_pg;
+ALTER FUNCTION new_stock_data.find_symbols_gte_min_split_adj_close(TEXT, NUMERIC, DATE) OWNER TO data_updater_pg;
--- pg_create_new_stock_tables.sql
+++ pg_create_new_stock_tables.sql
@@ -18,17 +18,24 @@ CREATE TABLE new_stock_data.current_data ( -- noqa: disable=L008
     high NUMERIC(12,4) NULL DEFAULT '0',
     low NUMERIC(12,4) NULL DEFAULT '0',
     close_p NUMERIC(12,4) NULL DEFAULT '0',
+    adjclose NUMERIC(12,4) NULL DEFAULT '0',
     volume BIGINT NULL DEFAULT '0',
-    adjopen NUMERIC(12,4) NULL DEFAULT '-1',
-    adjhigh NUMERIC(12,4) NULL DEFAULT '-1',
-    adjlow NUMERIC(12,4) NULL DEFAULT '-1',
-    adjclose NUMERIC(12,4) NULL DEFAULT '-1',
-    adjvolume BIGINT NULL DEFAULT '-1',
+    split_adj_open NUMERIC(12,4) NULL DEFAULT '-1',
+    split_adj_high NUMERIC(12,4) NULL DEFAULT '-1',
+    split_adj_low NUMERIC(12,4) NULL DEFAULT '-1',
+    split_adj_close NUMERIC(12,4) NULL DEFAULT '-1',
+    split_adj_volume BIGINT NULL DEFAULT '-1',
     alternate_close NUMERIC(12,4) NULL DEFAULT '0',
     close_up_or_down SMALLINT, -- noqa: enable=L008
 
     PRIMARY KEY (symbol, date)
 );
+
+-- don't create this index since it seems to greatly slow down
+-- data loads
+
+-- CREATE INDEX symbol_idx ON new_stock_data.current_data (symbol);
+
 ALTER TABLE new_stock_data.current_data OWNER TO data_updater_pg;
 
 DROP TABLE IF EXISTS new_stock_data.index_history;
@@ -79,33 +86,33 @@ $$;
 
 ALTER FUNCTION public.new_compute_close_up_or_down() OWNER TO data_updater_pg;
 
-DROP FUNCTION IF EXISTS public.new_compute_adjclose_up_or_down() CASCADE;
+DROP FUNCTION IF EXISTS public.new_compute_split_adj_close_up_or_down() CASCADE;
 
-CREATE FUNCTION public.new_compute_adjclose_up_or_down()
+CREATE FUNCTION public.new_compute_split_adj_close_up_or_down()
 RETURNS TRIGGER
 LANGUAGE plpgsql
 AS
 $$
 BEGIN
-    IF NEW.adjopen = -1 THEN
-        NEW.adjopen = NEW.open_p;
+    IF NEW.split_adj_open = -1 THEN
+        NEW.split_adj_open = NEW.open_p;
     END IF;
-    IF NEW.adjhigh = -1 THEN
-        NEW.adjhigh = NEW.high;
+    IF NEW.split_adj_high = -1 THEN
+        NEW.split_adj_high = NEW.high;
     END IF;
-    IF NEW.adjlow = -1 THEN
-        NEW.adjlow = NEW.low;
+    IF NEW.split_adj_low = -1 THEN
+        NEW.split_adj_low = NEW.low;
     END IF;
-    IF NEW.adjclose = -1 THEN
-        NEW.adjclose = NEW.close_p;
+    IF NEW.split_adj_close = -1 THEN
+        NEW.split_adj_close = NEW.close_p;
     END IF;
-    IF NEW.adjvolume = -1 THEN
-        NEW.adjvolume = NEW.volume;
+    IF NEW.split_adj_volume = -1 THEN
+        NEW.split_adj_volume = NEW.volume;
     END IF;
 
-    IF NEW.adjopen > NEW.adjclose THEN
+    IF NEW.split_adj_open > NEW.split_adj_close THEN
         NEW.close_up_or_down := -1; -- closed down
-    ELSIF NEW.adjclose > NEW.adjopen THEN
+    ELSIF NEW.split_adj_close > NEW.split_adj_open THEN
         NEW.close_up_or_down := 1; -- close_p up
     ELSE
         NEW.close_up_or_down := 0; -- no change
@@ -114,21 +121,21 @@ BEGIN
 END;
 $$;
 
-ALTER FUNCTION public.new_compute_adjclose_up_or_down() OWNER TO data_updater_pg;
+ALTER FUNCTION public.new_compute_split_adj_close_up_or_down() OWNER TO data_updater_pg;
 
 DROP TRIGGER IF EXISTS new_current_data_insert_trg ON new_stock_data.current_data;
 
 CREATE TRIGGER new_current_data_insert_trg -- noqa: disable=L003
     BEFORE INSERT ON new_stock_data.current_data
     FOR EACH ROW
-        EXECUTE PROCEDURE public.new_compute_adjclose_up_or_down();
+        EXECUTE PROCEDURE public.new_compute_split_adj_close_up_or_down();
 
 DROP TRIGGER IF EXISTS new_current_data_update_trg ON new_stock_data.current_data;
 
 CREATE TRIGGER new_current_data_update_trg
     BEFORE UPDATE ON new_stock_data.current_data
     FOR EACH ROW
-        EXECUTE PROCEDURE public.new_compute_adjclose_up_or_down();
+        EXECUTE PROCEDURE public.new_compute_split_adj_close_up_or_down();
 
 DROP TRIGGER IF EXISTS new_index_history_insert_trg ON new_stock_data.index_history;
 
@@ -143,4 +150,4 @@ CREATE TRIGGER new_index_history_update_trg
     BEFORE UPDATE ON new_stock_data.index_history
     FOR EACH ROW
        EXECUTE PROCEDURE public.new_compute_close_up_or_down();
-    -- noqa: enable=L003
+-- noqa: enable=L003
