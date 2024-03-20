7d4c0c79aad4dade64ceb5541cc9ca21b1f54e09
David P. Riedel <davidp.riedel@gmail.com>
2024-03-20 14:32:28 -0400

    start refactoring

---
+++ src/Streamer.cpp
@@ -0,0 +1,17 @@
+// =====================================================================================
+//
+//       Filename:  Streamer.cpp
+//
+//    Description:  Wrapper for streaming data providers
+//
+//        Version:  1.0
+//        Created:  03/20/2024 01:51:32 PM
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  David P. Riedel (), driedel@cox.net
+//   Organization:
+//
+// =====================================================================================
+
+#include "Streamer.h"
